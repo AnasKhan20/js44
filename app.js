e7c34bf2bdc663cbc059df3ed67acf1349e92a0e
AnasKhan20 <143949011+AnasKhan20@users.noreply.github.com>
2023-09-18 02:22:00 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,104 @@
+// CHAPTER  17-20 Home
+
+// Question 1
+
+// for(i=1;i<=10;i++){
+//     console.log("welcome",i)
+// }
+
+
+// Question 2
+
+// for(i=1;i<=12;i++){
+//     console.log("hello",i)
+// }
+
+// Question 3
+
+// for(i=0;i<=4;i++){
+//     document.write("hello"+"<br>")
+// }
+
+// Question 4
+
+// for(a=0;a<100;a++){
+//     document.write("hi" + "<br>")
+// }
+
+// Questiokn 5
+
+// for(i=5;i>2;--i){
+//     document.write("hi"+"<br>")
+// }
+
+// Question 6
+
+// var arr = ["tiger",12,"cat"]
+// console.log(arr)
+
+
+// Question 9
+
+// for(i=1;i<=10;i++){
+//     alert(i)
+// }
+
+// Question 10
+
+// var userName = prompt("Enter your name")
+
+// arr = ["anas","rehman","bilal"]
+
+// for(i=0;i<=5;i++){
+//     if(userName === arr[0]){
+
+//         alert("Enter")
+//     }
+// else{
+//     alert("please write correct username")
+// }
+// }
+
+
+// Question 11
+
+// var matchFound = false;
+// for(var i=0;i<List.length;i++){
+//     if(userInput===list[i]){
+//         alert("match found");
+//         matchFound = false;
+//     }
+//     else{
+//         alert("match not found")
+//         matchFound = true
+//     }
+// }
+
+// Question 12
+
+
+
+
+// Class assigment
+
+
+// Question 3
+
+// for(i=1;i<=10;i++){
+//     document.write(i+"<br>")
+// }
+
+
+// Question 4
+
+// var number = +prompt("Enter a number")
+// var length = +prompt("Enter a length")
+
+// for(i=1;i<=length;i++){
+//     document.write(number + "x" + i + "="+number*i + "<br>")
+// }
+
+// question 5
+
+var arr = ["apple","banana","mango","orange","strawberry"]
+
